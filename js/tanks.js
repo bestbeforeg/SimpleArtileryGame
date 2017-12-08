441a03bbc43578be33dd7dc3da0d8bf372116d2a
Georgi Daskalov <bestbeforeg@gmail.com>
2017-12-08 18:41:27 +0200

    adds player info on top and fixes player power going below zero

--- js/tanks.js
+++ js/tanks.js
@@ -1,27 +1,29 @@
 function game() {
 	let canvas = document.getElementById('canvas'),
+		playerInfo = document.getElementById('playerInfo'),
 		ctx = canvas.getContext('2d'),
 		gravity = 9.81,
 		turn = 1,
 		isGameOver = false,
 		shootTimer,
 		shell,
+		isShooting = false;
 		Img = {
 			player1 : new Image(),
 			player2 : new Image(),
 			shell : new Image(),
 			map : new Image(),
+			explode : new Image(),
 		};
 	Img.player1.src = './img/tank.png';
 	Img.player2.src = './img/tank2.png';
 	Img.shell.src = './img/shell.png';
 	Img.map.src = './img/map.png';
+	Img.explode.src = './img/explode.png';
 
 	ctx.mozImageSmoothingEnabled = false; //better graphics for pixel art
 	ctx.msImageSmoothingEnabled = false;
 	ctx.imageSmoothingEnabled = false;
-
-
 		
 	let	player1 = {
 			name : 'Player 1',
@@ -42,14 +44,59 @@ function game() {
 			power : 80,
 			aimPosition : 0,
 			img : Img.player2,
-		};	
+		};
 
-	let	curPlayer;
+	let	curPlayer = player1;
+	playerInfo.innerHTML = curPlayer.name;
+
+	Img.map.onload = function () {
+		ctx.drawImage(Img.map, 0, 0);
+		position(player1);
+		draw(player1);
+		position(player2);
+		draw(player2);
+	}
+
+	// ctx.save();
+	// ctx.font = "22px Arial";
+	// ctx.fillStyle = 'white';
+	// ctx.fillText('curPlayer.name',10,50);
+	// ctx.restore();
+
+	// let startGame = function(){
+	// 	turn = 1,
+	// 	isGameOver = false,
+	// 	isShooting = false,
+	// 	player1 = {
+	// 		name : 'Player 1',
+	// 		x: 0,
+	// 		y: 0,	
+	// 		angle : 180,
+	// 		stepAngle : 22.5,
+	// 		power : 80,
+	// 		aimPosition : 0,
+	// 		img : Img.player1,
+	// 	},
+	// 	player2 = {
+	// 		name : 'Player 2',
+	// 		x: 0,
+	// 		y: 0,
+	// 		angle : 180,
+	// 		stepAngle : 22.5,
+	// 		power : 80,
+	// 		aimPosition : 0,
+	// 		img : Img.player2,
+	// 	};
+	// }
+
 	let round = function () {
+
 		if(turn % 2 == 0)
 			curPlayer = player2;
 		else
 			curPlayer = player1;
+
+		playerInfo.innerHTML = curPlayer.name + '/Power ' + curPlayer.power;
 		
 		shell = {
 			posX : [6,8,19,28,32,39,45,55,61],
@@ -61,21 +108,13 @@ function game() {
 		};
 	}
 
-	Img.map.onload = function () {
-		ctx.drawImage(Img.map, 0, 0);
-		position(player1);
-		draw(player1);
-		position(player2);
-		draw(player2);
-	}	
-
 	let draw = function (player){
 		ctx.drawImage(player.img, player.aimPosition * (player1.img.width / 9), 0, player1.img.width / 9, player1.img.height, player.x, player.y, player1.img.width / 9, player1.img.height);
 	}
 
 	let position = function(player){
 		let randomPos = Math.round(Math.random() * (mapPositions.length -1));
-		player.x = mapPositions[randomPos][1] * 32;
+		player.x = mapPositions[randomPos][1] * 32
 		player.y = mapPositions[randomPos][0] * 32 - (player.img.height - 17);
 		player.width = player.img.width / 9;
 		player.height = player.img.height - 17;
@@ -93,8 +132,6 @@ function game() {
 		}
 	};
 
-
-	let isShooting = false;
 	let shoot = function(player){
 		//calculate trajectory
 		let vx = player.power * Math.cos(player.angle/180*Math.PI),
@@ -108,7 +145,7 @@ function game() {
 		//draw shell
 		ctx.save();
 		ctx.clearRect(0, 0, 800, 600);
-		ctx.drawImage(Img.map, 0, 0);
+		ctx.drawImage(Img.map, 0, 0)
 		draw(player1);
 		draw(player2);
 		ctx.drawImage(Img.shell, shell.framePosition * Img.shell.width / 19, 0, Img.shell.width / 19, Img.shell.height, shell.x, shell.y, Img.shell.width / 19, Img.shell.height);
@@ -150,8 +187,6 @@ function game() {
 		}
 	}
 
-	
-
 	let aim = function(event, player){
 		if (event.keyCode == 68) {
 			player.aimPosition ++;
@@ -173,7 +208,7 @@ function game() {
 
 		ctx.save();
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.drawImage(Img.map, 0, 0);
+		ctx.drawImage(Img.map, 0, 0)
 		draw(player1);
 		draw(player2);
 		ctx.restore();
@@ -193,10 +228,16 @@ function game() {
 			aim(event, curPlayer);
 		else if (event.keyCode == 68 && !isGameOver  && !isShooting)
 			aim(event, curPlayer);
-		else if (event.keyCode == 87 && !isGameOver  && !isShooting)
+		else if (event.keyCode == 87 && !isGameOver  && !isShooting){
 			curPlayer.power+=5;
-		else if (event.keyCode == 83 && !isGameOver  && !isShooting)
+			playerInfo.innerHTML = curPlayer.name + '/Power ' + curPlayer.power;
+		}
+		else if (event.keyCode == 83 && !isGameOver  && !isShooting){
 			curPlayer.power-=5;
+			if(curPlayer.power < 0)
+				curPlayer.power = 0;
+			playerInfo.innerHTML = curPlayer.name + '/Power ' + curPlayer.power;
+		}
 		else if (event.keyCode == 32 && !isGameOver  && !isShooting)
 			shootTimer = setInterval( function () {
 				shoot(curPlayer);
@@ -219,18 +260,15 @@ function game() {
 		//params for explosion	
 		let explodeFrame = 0,
 			explodeCount = 0,
-			explodeImg = new Image(),
 			explodeWidth = 328,
 			explodeHeight = 81;
-		explodeImg.src = './img/explode.png';
 	
 		let explosion = setInterval(function(){
 			ctx.save();
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
 			ctx.drawImage(Img.map, 0, 0);
-
 			//draw explosion
-			ctx.drawImage(explodeImg, explodeFrame * explodeWidth / 5, explodeCount * explodeHeight, explodeWidth / 5, explodeHeight, killedPlayer.x, killedPlayer.y - 20, explodeWidth / 5, explodeHeight);
+			ctx.drawImage(Img.explode, explodeFrame * explodeWidth / 5, explodeCount * explodeHeight, explodeWidth / 5, explodeHeight, killedPlayer.x, killedPlayer.y - 20, explodeWidth / 5, explodeHeight);
 
 			draw(curPlayer);
 			ctx.restore();
@@ -243,13 +281,15 @@ function game() {
 			if(explodeFrame == 25)
 				clearInterval(explosion);
 
-		}, 80);
+		}, 40);
 	}
 
+	//map from tiled.com
 	let array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 70, 71, 72, 73, 74, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 70, 71, 72, 73, 74, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 70, 71, 72, 73, 74, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 	let array2D = [],
 		mapPositions = [];
 
+	//generate matrix for map
 	for(let i = 0 ; i < 14; i++){
 		array2D[i] = [];
 		for(let j = 0 ; j < 28; j++){
